98f60d6d85e7dde4ebc8aa217b879d662736b51f
soberanoJr <tonysoberano@gmail.com>
2023-08-02 11:15:08 +0000

    Tipos numéricos, char, bool, var

---
+++ src/main/java/com/example/Tipos.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Tipos {
+    public static void main(String[] args) {
+        byte byteNumber = 18;
+        double doubleNumber = byteNumber;
+        char asciiChar = 77;
+        char gender = 'M';
+        boolean choice = true;
+        var number = 1;
+        var anotherNumber = 3.14;
+        var character = 'a';
+        var bool = true;
+        System.out.println("-> byte number: " + byteNumber);
+        System.out.println("-> double number: " + doubleNumber);
+        System.out.println("-> ascii char: " + asciiChar);
+        System.out.println("-> gender: " + gender);
+        System.out.println("-> choice: " + choice);
+        System.out.println("-> var (number): " + number);
+        System.out.println("-> var (another number): " + anotherNumber);
+        System.out.println("-> var (character): " + character);
+        System.out.println("-> var (bool): " + bool);
+    }
+}
