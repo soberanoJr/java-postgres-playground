3a18f4c2243805fe7a8a437fd62dcc68258911dc
soberanoJr <tonysoberano@gmail.com>
2023-08-04 10:54:03 +0000

    Constructors

--- src/main/java/com/example/AppClasses.java
+++ src/main/java/com/example/AppClasses.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class AppClasses {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente();
-        cliente.setRenda(1250.75);
-        cliente.setSexo('M');
-        cliente.setAnoNascimento(1950);
+        Cliente cliente = new Cliente('F', 1979, 11943.57);
+        //cliente.setRenda(1250.75);
+        //cliente.setSexo('M');
+        //cliente.setAnoNascimento(1950);
         System.out.print("-> Renda: R$" + cliente.getRenda());
         System.out.print(" | Sexo: " + cliente.getSexo());
         System.out.println(" | Ano de Nascimento: " + cliente.getAnoNascimento());
--- src/main/java/com/example/Cliente.java
+++ src/main/java/com/example/Cliente.java
@@ -5,29 +5,35 @@ public class Cliente {
     private int anoNascimento;
     private double renda;
 
+    public Cliente(char sexo, int anoNascimento, double renda){
+        this.sexo = sexo;
+        this.anoNascimento = anoNascimento;
+        this.renda = renda;
+    }
+
     public char getSexo() {
         return sexo;
     }
 
-    public void setSexo(char sexo) {
+    /* public void setSexo(char sexo) {
         this.sexo = sexo;
-    }
+    } */
 
     public int getAnoNascimento() {
         return anoNascimento;
     }
 
-    public void setAnoNascimento(int anoNascimento) {
+    /* public void setAnoNascimento(int anoNascimento) {
         this.anoNascimento = anoNascimento;
-    }
+    } */
 
     public double getRenda(){
         return renda;
     }
 
-    public void setRenda(double renda){
+    /* public void setRenda(double renda){
         if(renda >= 0)
            this.renda = renda;
         else System.out.println("Renda < 0?");
-    }
+    } */
 }
