7bbe45c3bae646e0581e70038ad370e09db4438e
soberanoJr <tonysoberano@gmail.com>
2023-08-04 22:53:08 +0000

    String

--- src/main/java/com/example/AppClasses.java
+++ src/main/java/com/example/AppClasses.java
@@ -2,12 +2,12 @@ package com.example;
 
 public class AppClasses {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente('F', 1979, 11943.57);
-        //cliente.setRenda(1250.75);
-        //cliente.setSexo('M');
-        //cliente.setAnoNascimento(1950);
-        System.out.print("-> Renda: R$" + cliente.getRenda());
-        System.out.print(" | Sexo: " + cliente.getSexo());
-        System.out.println(" | Ano de Nascimento: " + cliente.getAnoNascimento());
-    }   
+        Cliente alice = new Cliente("Alice", 'F', 1979, 17155.50);
+        Cliente bob = new Cliente("Bob", 'M', 1983, 11943.57);
+        Cliente charlie = new Cliente("Charlie", 'M', 1981, 13887.26);
+
+        System.out.println(alice.getName() + " | " + alice.getSexo() + " | " + alice.getRenda() + " | " + alice.getAnoNascimento());
+        System.out.println(bob.getName() + " | " + bob.getSexo() + " | " + bob.getRenda() + " | " + bob.getAnoNascimento());
+        System.out.println(charlie.getName() + " | " + charlie.getSexo() + " | " + charlie.getRenda() + " | " + charlie.getAnoNascimento());
+        }   
 }
--- src/main/java/com/example/Cliente.java
+++ src/main/java/com/example/Cliente.java
@@ -1,34 +1,48 @@
 package com.example;
 
 public class Cliente {
+    private String nome;
     private char sexo;
     private int anoNascimento;
     private double renda;
 
-    public Cliente(char sexo, int anoNascimento, double renda){
+    public Cliente(String nome, char sexo, int anoNascimento, double renda){
+        this.nome = nome;
         this.sexo = sexo;
         this.anoNascimento = anoNascimento;
         this.renda = renda;
+    }
+
+    public String getName() {
+        return nome;
+    }
+
+    public void setName(String name) {
+        this.nome = name.trim();
     }
 
     public char getSexo() {
         return sexo;
     }
 
-    /* public void setSexo(char sexo) {
+    public void setSexo(char sexo) {
         this.sexo = sexo;
-    } */
+    }
 
     public int getAnoNascimento() {
         return anoNascimento;
     }
 
-    /* public void setAnoNascimento(int anoNascimento) {
+    public void setAnoNascimento(int anoNascimento) {
         this.anoNascimento = anoNascimento;
-    } */
+    }
 
     public double getRenda(){
         return renda;
+    }
+
+    public void setRenda(double renda) {
+        this.renda = renda;
     }
 
     /* public void setRenda(double renda){
